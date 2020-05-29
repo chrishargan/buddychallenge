33b1631246ce2bfc9d79e895e382e9856243fbee
JensDN <jens.deneef@gmail.com>
2020-05-29 16:25:38 +0200

    Update from Jens

--- form.js
+++ form.js
@@ -3,3 +3,22 @@ const email = document.getElementById('email');
 const subject = document.getElementById('subject');
 const message = document.getElementById('message');
 const formButton = document.getElementById('formButton');
+
+
+
+name.addEventListener('change', stateHandle);
+email.addEventListener('change', stateHandle);
+subject.addEventListener('change', stateHandle);
+message.addEventListener('change', stateHandle);
+
+
+function stateHandle() {
+    if (name.value === '' || email.value === '' || subject.value === '' || message.value === '') {
+        formButton.disabled = true
+        formButton.className = "btn btn-primary disabled"
+    } else {
+        formButton.disabled = false
+        formButton.className = "btn btn-primary"
+    }
+}
+
