335f1b6a19810b68e96b2d7055196e9b92a8f129
JensDN <jens.deneef@gmail.com>
2020-05-29 14:46:56 +0200

    Update Tabs Jens

---
+++ tabs.js
@@ -0,0 +1,14 @@
+function onTabClick(event) {
+    let activeTab = document.querySelectorAll('.nav-link.active');
+    activeTab.forEach(function(tab) {
+        tab.className = tab.className.replace('active', '');
+    });
+    event.target.className += ' active';
+    document.querySelectorAll('[role="tabpanel"]').forEach(function (el){
+        el.classList.remove("active");
+    });
+    document.getElementById(event.target.href.split('#')[1]).className += ' show active';
+}
+const element = document.getElementById('info');
+
+element.addEventListener('click', onTabClick, false);
